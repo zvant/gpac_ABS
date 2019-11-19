50a9764d97edfa4f246e13e78c26fb4b1f80cb3c
Zekun Zhang <kzz1994@gmail.com>
2019-11-19 17:33:49 -0500

    restrict bandwidth at server

--- runclient.bat
+++ runclient.bat
@@ -1,2 +1,3 @@
-bin\x64\release\mp4client.exe -run-for 80 -exit -c GPAC.cfg -logs all@error:dash@debug -log-file dash.log "http://127.0.0.1/dash/dash.mpd"
+rem bin\x64\release\mp4client.exe -run-for 120 -exit -c GPAC.cfg -logs all@error:dash@debug -log-file dash.log "http://127.0.0.1/dash/dash.mpd"
 rem bin\x64\release\mp4client.exe -h
+bin\x64\release\mp4client.exe -run-for 120 -exit -c GPAC.cfg -logs all@error:dash@debug -log-file dash.log "http://172.17.217.51/dash/dash.mpd"
